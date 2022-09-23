87905d80621af16103e8d5a1898187e907b8f7a4
Oliver Epper <oliver.epper@gmail.com>
2022-09-23 10:42:18 +0200

    better xcframework

--- Package.swift
+++ Package.swift
@@ -12,10 +12,15 @@ let package = Package(
             name: "SwiftSIP", targets: ["SwiftSIP"]),
     ],
     targets: [
-        .binaryTarget(name: "libpjproject", path: "libpjproject.xcframework"),
-        .systemLibrary(name: "Cpjproject", pkgConfig: "pjproject-apple-platforms-SPM"),
-        .target(name: "Controller", dependencies: ["libpjproject","Cpjproject"], cxxSettings: [
+        .binaryTarget(name: "libpjproject", url: "https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.8.1/libpjproject.xcframework-0.8.1.zip", checksum: "d47671ad29cab859b6d2db4614877c22c20257b7047985447a9794f1421e5be1"),
+        .target(name: "Controller", dependencies: ["libpjproject"], cxxSettings: [
             .define("PJ_AUTOCONF")
+        ], linkerSettings: [
+            .linkedFramework("Network"),
+            .linkedFramework("Security"),
+            .linkedFramework("CoreAudio"),
+            .linkedFramework("AVFoundation"),
+            .linkedFramework("AudioToolbox")
         ]),
         .target(name: "SwiftSIP", dependencies: ["Controller"], cxxSettings: [
             .define("PJ_AUTOCONF")
@@ -26,3 +31,4 @@ let package = Package(
     ],
     cxxLanguageStandard: .cxx20
 )
+
