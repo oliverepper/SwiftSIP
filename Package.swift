32372448079b9b21f49e864281c936b55dce2533
Oliver Epper <oliver.epper@gmail.com>
2022-11-07 17:00:57 +0100

    pjproject-apple-platforms v0.10.1

--- Package.swift
+++ Package.swift
@@ -12,8 +12,7 @@ let package = Package(
             name: "SwiftSIP", targets: ["SwiftSIP"]),
     ],
     targets: [
-        .binaryTarget(name: "libpjproject", url: "https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.10/libpjproject.xcframework-0.10.zip", checksum: "2e728e891c07d7bd347fa527b655120acb515a361b099e2f543f1ee98ecc448f"),
-//        .binaryTarget(name: "libpjproject", path: "libpjproject.xcframework"),
+        .binaryTarget(name: "libpjproject", url: "https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.10.1/libpjproject.xcframework.zip", checksum: "eb3816a874f7fdcf47ce68b8efab924ee563d4534cbbb8ff4c88ea522b775f8c"),
         .target(name: "Controller", dependencies: ["libpjproject"], cxxSettings: [
             .define("PJ_AUTOCONF")
         ], linkerSettings: [
